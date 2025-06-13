6ab92bac9b0dcb2cb7e1cb6fbca3cd4feff95094
Cristian Gomez <ycamigo123@gmail.com>
2025-06-13 15:17:04 -0500

    fix: resolve database name conflicts in connection files
    Update database name from 'hermesbeta' to 'hermes002' in both connection files to maintain consistency and resolve merge conflicts

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -3,11 +3,7 @@
 class Conexion{
 
     static public function conectar(){
-<<<<<<< HEAD
         $link = new PDO("mysql:host=localhost;dbname=hermes002", "root", "");
-=======
-        $link = new PDO("mysql:host=localhost;dbname=hermesbeta", "root", "");
->>>>>>> 4077495965506af35bd91684ad655bc3b2f8dd5b
         $link -> exec("set names utf8");
         return $link;
     }
